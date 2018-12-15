7ae1038676738fb2680a0347a9d18e770e9c3800
Rodin <rodinandrey@mail.ru>
2018-12-15 07:46:31 +0300

    Initial Commit

---
+++ Lessons/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lessons
+//
+//  Created by Андрей Родин on 15/12/2018.
+//  Copyright © 2018 Андрей Родин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
